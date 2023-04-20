119a1f19636a69ffc9d72e15bb52567f2528b78b
Mike Le <mike.le@live.com>
2023-04-20 14:06:40 -0700

    add resist to keyboardinterrupt

--- todo.py
+++ todo.py
@@ -1,25 +1,45 @@
 # main.py
+
 import os
 from task_manager import add_task, display_tasks, remove_task
 
-def main():
-    while True:
+def clear_screen():
+    if os.name == "nt":  # Windows
+        os.system("cls")
+    else:  # macOS and Linux
         os.system("clear")
+
+def main():
+    last_message = ""
+
+    while True:
+        clear_screen()
+        if last_message:
+            print(last_message)
+            last_message = ""
+
         print("\nTo-do List Manager")
         print("-------------------")
         display_tasks()
-        user_input = input("Enter anew task, 'r' to remove a task, or 'q' to quit: ").strip()
 
-        if user_input.lower() == 'q':
-            break
-        elif user_input.lower() == 'r':
-            try:
-                task_number = int(input("Enter the task number to remove: "))
-                remove_task(task_number)
-            except ValueError:
-                print("Invalid input. Please enter a valid task number.")
-        else:
-            add_task(user_input)
+        try:
+            user_input = input("Enter a new task, 'r' to remove a task, or 'q' to quit: ").strip()
+
+            if user_input.lower() == 'q':
+                break
+            elif user_input.lower() == 'r':
+                try:
+                    task_number = int(input("Enter the task number to remove: "))
+                    remove_task(task_number)
+                    last_message = "Task removed successfully."
+                except ValueError:
+                    last_message = "Invalid input. Please enter a valid task number."
+            else:
+                add_task(user_input)
+                last_message = "Task added successfully."
+        except KeyboardInterrupt:
+            last_message = "Interrupt detected. Restarting task input."
+            continue
 
 if __name__ == "__main__":
     main()
